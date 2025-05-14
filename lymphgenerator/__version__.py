dde4c9b6c5f6a0896f2e4c54bbb62ce23737ebe7
Kdreval <k.dreval@gmail.com>
2025-05-14 13:01:12 -0700

    minor version bump

--- lymphgenerator/__version__.py
+++ lymphgenerator/__version__.py
@@ -6,7 +6,7 @@ Information about the current version of the py-package-template package.
 
 __title__ = "lymphgenerator"
 __description__ = "Functions for running LymphGenerator"
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "KD"
 __author_email__ = "kdreval@sfu.ca"
 __license__ = "MIT"
